e1e5fe3c96f0f992f1f563a5b55084e5c586ae7f
Ubuntu <ubuntu@ip-172-31-45-139.ap-south-1.compute.internal>
2020-08-09 02:43:04 +0000

    while loop update

---
+++ for.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pets="Dog Cat Cow Mouse"
+for pet in $pets
+do
+echo "Hello" $pet
+done
--- whileloop.sh
+++ whileloop.sh
@@ -3,4 +3,9 @@ count=1
 while [ $count -le 10 ]
 do
   echo $((count++))
+if [ $count == 6 ]
+then 
+echo "break"
+break
+fi
 done
